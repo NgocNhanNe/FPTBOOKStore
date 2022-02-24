082dea1fc6d67257a04b459343656edbeb4b0f6d
nhannngcc19209 <nhannngcc19209@fpt.edu.vn>
2022-02-24 19:33:05 +0700

    Update manage user page

--- FPTBook/Controllers/ManageUserController.cs
+++ FPTBook/Controllers/ManageUserController.cs
@@ -21,21 +21,6 @@ namespace FPTBook.Controllers
             return View(db.Users.ToList());
         }
 
-        // GET: ManageUser/Details/5
-        public ActionResult Details(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            User user = db.Users.Find(id);
-            if (user == null)
-            {
-                return HttpNotFound();
-            }
-            return View(user);
-        }
-
         // GET: ManageUser/Create
         public ActionResult Create()
         {
